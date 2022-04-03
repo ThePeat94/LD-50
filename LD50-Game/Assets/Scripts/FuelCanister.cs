46bdb3bdeec24ff94152c5866e84715e6b0b3679
Hendrik Rakemann <hendrik.rakemann@gmx.de>
2022-04-03 11:35:49 +0200

    fuel

---
+++ LD50-Game/Assets/Scripts/FuelCanister.cs
@@ -0,0 +1,20 @@
+﻿using Nidavellir.ResourceControllers;
+using UnityEngine;
+
+namespace Nidavellir
+{
+    public class FuelCanister : MonoBehaviour
+    {
+        [SerializeField] private float m_amount;
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent<FuelResourceController>(out var fuelController))
+            {
+                fuelController.ResourceController.Add(this.m_amount);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
