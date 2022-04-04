6b3306cce1d99ee2f80074298de5dccc26f871b2
Hendrik Rakemann <hendrik.rakemann@gmx.de>
2022-04-04 15:16:22 +0200

    Integrated Logo and UI

---
+++ LD50-Game/Assets/Scripts/UI/RotateUiElement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Nidavellir.UI
+{
+    public class RotateUiElement : MonoBehaviour
+    {
+        [SerializeField] private float m_rotationSpeed;
+
+        private void Update()
+        {
+            this.transform.RotateAround(this.transform.position, Vector3.up, this.m_rotationSpeed * Time.deltaTime);
+        }
+    }
+}
